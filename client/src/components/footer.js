5601eb58e5a8e692d6c4c76ea805c4f436798c47
Jamil Ahmed <Compitu1@gmail.com>
2018-10-12 17:40:31 -0400

    Exchnage Pages done and Night View issue resolved

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,116 +1,70 @@
-import React, { Component } from "react";
-import logo_blue from "./imgs/logo_blue.png";
+import React, {Component} from 'react';
+import logo_blue from './imgs/logo_blue.png';
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="footer">
-        <div className="container clearfix">
-          <div className="widget">
-            <img src={logo_blue} />
-            <p>
-              Cryptocurrency investment is subject to high market risk, please
-              make your investments cautiously.
-            </p>
-            <span>Copyright 2018 UberDex Inc.</span>
-          </div>
+class Footer extends Component{
+    render(){
+       
+        return(
+            <div className="footer">
+                <div className="container clearfix">
+                    <div className="widget">
+                        <img src={logo_blue} />
+                        <p>Cryptocurrency investment is subject to high market risk, please make your investments cautiously.</p>
+                        <span>Copyright 2018 UberDex Inc. All Right Reserved.</span>
+                    </div>
+                    
+                    <div className="widget">
+                        <h5>Products</h5>
+                        <ul>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                        </ul>
+                    </div>
+                    
+                    <div className="widget">
+                        <h5>Learn</h5>
+                        <ul>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                            <li><a href="/">Lorem Ipsum</a></li>
+                        </ul>
+                    </div>
+                    
+                    <div className="widget">
+                        <h5>Company</h5>
+                        <ul>
+                            <li><a href="/">About </a></li>
+                            <li><a href="/">Careers</a></li>
+                            <li><a href="/">Press</a></li>
+                            <li><a href="/">Open Source</a></li>
+                            <li><a href="/">Legal & Privacy</a></li>
+                            <li><a href="/">Support</a></li>
+                        </ul>
+                    </div>
 
-          <div className="widget">
-            <h5>Products</h5>
-            <ul>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-            </ul>
-          </div>
+                    <div className="widget">
+                        <h5>Social</h5>
+                        <ul>
+                            <li><a href="/">Blog </a></li>
+                            <li><a href="/">Twitter</a></li>
+                            <li><a href="/">Facebook</a></li>
+                        </ul>
+                    </div>
 
-          <div className="widget">
-            <h5>Learn</h5>
-            <ul>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-              <li>
-                <a href="/">Lorem Ipsum</a>
-              </li>
-            </ul>
-          </div>
 
-          <div className="widget">
-            <h5>Company</h5>
-            <ul>
-              <li>
-                <a href="/">About </a>
-              </li>
-              <li>
-                <a href="/">Careers</a>
-              </li>
-              <li>
-                <a href="/">Press</a>
-              </li>
-              <li>
-                <a href="/">Open Source</a>
-              </li>
-              <li>
-                <a href="/">Legal & Privacy</a>
-              </li>
-              <li>
-                <a href="/">Support</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="widget">
-            <h5>Social</h5>
-            <ul>
-              <li>
-                <a href="/">Blog </a>
-              </li>
-              <li>
-                <a href="/">Twitter</a>
-              </li>
-              <li>
-                <a href="/">Facebook</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Footer;
